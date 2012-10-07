2c8d413bd5e57e5b22869c893f37728aa320f6ec
sulaco <sulaco@LOKHID-DESKTOP.analogbrain.com>
2012-10-07 18:10:21 +0200

    base 0 master extras

--- application/controllers/QformController.php
+++ application/controllers/QformController.php
@@ -1,28 +1,36 @@
 <?php
 
-class QformController extends Zend_Controller_Action
-{
+class QformController extends Zend_Controller_Action {
 
-    public function init()
-    {
-
+    public function init() {
+        $this->_helper->layout()->setLayout('qform');
+        if ($this->_request->isXmlHttpRequest()) {
+            $this->_helper->layout->disableLayout();
+            $this->_helper->viewRenderer->setNoRender(TRUE);
+        } else {
+            $this->view->config = (object) $this->getInvokeArg('bootstrap')->getOptions()['qform'];
+        }
     }
-    public function indexAction()
-    {
+
+    public function loginsubmitAction() {
+        
+        echo '{"success":true}';
+    }
+
+    public function indexAction() {
         
     }
-    public function qformAction()
-    {
-       $auth = Zend_Auth::getInstance();
-       if (!$auth->hasIdentity()) {
+
+    public function qformAction() {
+        $auth = Zend_Auth::getInstance();
+        if (!$auth->hasIdentity()) {
             $this->_redirect('qform/');
-       } 
-    }
-    public function adminAction()
-    {
-        
+        }
     }
 
+    public function adminAction() {
+        
+    }
 
 }
 
