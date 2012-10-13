da964260da7dee39dc2e1f22c66240a922bdf21b
sulaco <sulaco@LOKHID-DESKTOP.analogbrain.com>
2012-10-13 02:30:35 +0200

    Secure PinPad Idea  - keyboard layout 3

--- call.php
+++ call.php
@@ -6,8 +6,8 @@
     $_SESSION['sets'] = Array('7661','1975','1983');
  
  if(empty($_SESSION['clips'])) 
-    $_SESSION['clips'] = Array(0,0,0);
- 
+    $_SESSION['clips'] = Array(0,0,0,0);
+
  foreach($_SESSION['sets'] as $key => $value){
      if(strpos($_SESSION['numbers'],$value) > -1){
        $_SESSION['clips'][$key] = 1;  
@@ -17,6 +17,8 @@
      $_SESSION['clips'] = Array(0,0,0);
      session_destroy();
  }
+ if($_SESSION['clips'] == Array(1,1,1,0))
+    $_SESSION['clips'] = Array(1,1,1,1,'http://analogbrain.com');
      
- echo json_encode(array_merge($_SESSION['clips'],Array($_SESSION['numbers'])));
+ echo json_encode($_SESSION['clips']);
 ?>
